cc70e348edaad8c56acff8367d4206d7bb970f2b
jbride <jbride2001@yahoo.com>
2016-07-13 10:28:12 -0400

    https://github.com/redhat-gpe/OPEN_Reporting/issues/44

--- commons/services/src/main/java/com/redhat/gpte/services/EmailServiceBean.java
+++ commons/services/src/main/java/com/redhat/gpte/services/EmailServiceBean.java
@@ -84,7 +84,7 @@ public class EmailServiceBean extends GPTEBaseServiceBean {
                 logger.error("isValidCamelMessage() no return email address provided");
                 return false;
             } else if (!validEmailSuffixes.contains(fromEmailSuffix))  {
-                logger.error("isValidCamelMessage() email address is invalid (must origin from *@redhat.com) : "+fromEmailSuffix);
+                logger.error("isValidCamelMessage() email address is invalid: "+fromEmailSuffix);
                 return false;
             }
         } else {
