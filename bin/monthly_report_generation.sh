49ec0c50b5f8de59782edbe1454ca780b33953a9
Scott Mangan <smangan@redhat.com>
2016-08-26 08:19:28 -0400

    https://github.com/redhat-gpe/OPEN_Reporting/issues/90

--- bin/monthly_report_generation.sh
+++ bin/monthly_report_generation.sh
@@ -1,5 +1,52 @@
 #!/bin/bash
-# Purpose:  Test script
+# Purpose:  Create monthly QvExport file for reporting team.
+#
 
-echo "hello"
+sleepAmount=1
 
+PROPS_FILE_LOCATION="properties/$ENVIRONMENT.properties"
+OUTPUT_LOG_FILE=/tmp/monthly_report_generation.log
+
+function help() {
+
+    echo -en "\n\nOPTIONS:";
+    echo -en "\n\t-env=[dev, test, prod]        REQUIRED: specify environment specific java system properties (as per properties directory at the root of this project)"
+    echo -en "\n\t--help                        This help manual"
+    echo -en "\n\nEXAMPLES:";
+    echo -en "\n\t./bin/monthly_report_generation.sh -env=prod          :   initiate QvEx[prt for all students in prod environment \n\n"
+}
+
+function readPropertiesFile() {
+
+    # source properties file
+
+  if [ -f "$PROPS_FILE_LOCATION" ]
+  then
+    echo "$PROPS_FILE_LOCATION found."
+
+    while IFS='=' read -r key value
+    do
+      key=$(echo $key | tr '.' '_')
+      key=$(echo $key | tr ':' '_')
+      eval "${key}='${value}'" > /dev/null 2>&1;
+
+    done < "$PROPS_FILE_LOCATION"
+
+  else
+    echo "$PROPS_FILE_LOCATION not found."
+  fi
+
+}
+
+function callQvExport() {
+    echo -en "\n\ncallQvExport() db userId = $lms_transactional_username\n";
+    mysql -u $lms_transactional_username -p$lms_transactional_password -h $HOSTNAME lms_transactional -e 'call refresh_QvExport;'
+}
+
+if [ ! -z "$HELP" ]; then
+    help
+else
+    readPropertiesFile
+    callQvExport
+    echo "Procedure called"
+fi
