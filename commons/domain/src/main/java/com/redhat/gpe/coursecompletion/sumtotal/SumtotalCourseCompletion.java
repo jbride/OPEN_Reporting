6048dce2afe44023d4db3864fc7dec80fae144d3
jbride <jbride2001@yahoo.com>
2016-10-04 14:47:56 -0600

    making some fields mandatory on sumtotal course completions

--- commons/domain/src/main/java/com/redhat/gpe/coursecompletion/sumtotal/SumtotalCourseCompletion.java
+++ commons/domain/src/main/java/com/redhat/gpe/coursecompletion/sumtotal/SumtotalCourseCompletion.java
@@ -40,7 +40,7 @@ public class SumtotalCourseCompletion implements java.io.Serializable{
     private String fullName;
     
 
-    @DataField(pos=2)
+    @DataField(pos=2, required=true)
     private String email;
     
     private String text3;
@@ -53,13 +53,13 @@ public class SumtotalCourseCompletion implements java.io.Serializable{
     
     private boolean isActive;
     
-    @DataField(pos=3)
+    @DataField(pos=3, required=true)
     private String activityLabel;
 
     @DataField(pos=4)
     private String activityName;
     
-    @DataField(pos=5)
+    @DataField(pos=5, required=true)
     private String activityCode;
     
     private boolean isCertification;
@@ -70,24 +70,24 @@ public class SumtotalCourseCompletion implements java.io.Serializable{
 
     private Date attemptStartDate = new Date();
     
-    @DataField(pos=6, pattern="MMM dd, yyyy")
+    @DataField(pos=6, pattern="MMM dd, yyyy", required=true)
     private Date attemptEndDate = new Date();
     
     
     // Sumtotal course completion spreadsheet now provides company name from SalesForce
     // This companyName is included because the information often times is not available in GPTE's IPA LDAP
     // This is the case for students that are in a Sales track and not a SE / Delivery track (thus student doesn't need a lab environment nor OPENTLC SSO
-    @DataField(pos=7)
+    @DataField(pos=7, required=true)
     private String organization;
     
     // Tier and type (not currently used)
     @DataField(pos=8)
     private String text4;
 
-    @DataField(pos=9)
+    @DataField(pos=9, required=true)
     private String userNumber;
 
-    @DataField(pos=10)
+    @DataField(pos=10, required=true)
     private String country;
     
     public SumtotalCourseCompletion() {
