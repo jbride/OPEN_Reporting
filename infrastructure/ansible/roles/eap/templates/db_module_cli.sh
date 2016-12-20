4d76bec0dbfad9889716503e992064b89b3f3bac
jbride <jbride2001@yahoo.com>
2016-12-20 03:48:19 -0700

    https://github.com/redhat-gpe/OPEN_Reporting/issues/87

--- infrastructure/ansible/roles/eap/templates/db_module_cli.sh
+++ infrastructure/ansible/roles/eap/templates/db_module_cli.sh
@@ -1,5 +1,3 @@
-batch
+# Create JBoss EAP module based on jdbc driver library installed from RHN
+module add --name=@@lms_trans_module_name@@ --resources=@@lms_trans_driver_jar_dir@@/@@lms_trans_driver_jar@@ --dependencies=javax.api,javax.transaction.api
 
-  module add --name=@@lms_trans_module_name@@ --resources=@@lms_trans_driver_jar_dir@@/@@lms_trans_driver_jar@@ --dependencies=javax.api,javax.transaction.api
-
-run-batch
