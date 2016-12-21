2c6b9adb888b29ca8d6ff5c7b2a876e38bc2f78b
jbride <jbride2001@yahoo.com>
2016-12-21 07:55:56 -0700

    https://github.com/redhat-gpe/OPEN_Reporting/issues/87

--- infrastructure/ansible/roles/eap/templates/eap_generic_cli.sh
+++ infrastructure/ansible/roles/eap/templates/eap_generic_cli.sh
@@ -1,3 +1,10 @@
 # Create JBoss EAP module based on jdbc driver library installed from RHN
 module add --name=@@lms_trans_module_name@@ --resources=@@lms_trans_driver_jar_dir@@/@@lms_trans_driver_jar@@ --dependencies=javax.api,javax.transaction.api
 
+batch
+
+# file logger should refresh after every bounce of server
+/subsystem=logging/periodic-rotating-file-handler=FILE:write-attribute(name=append,value=false)
+
+run-batch
+
