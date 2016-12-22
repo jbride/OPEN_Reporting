ad6c5f83fc48c083b5e6a894eab829fdc64a0fbf
jbride <jbride2001@yahoo.com>
2016-12-22 08:01:41 -0700

    https://github.com/redhat-gpe/OPEN_Reporting/issues/87

--- infrastructure/ansible/roles/eap/templates/lms_trans_cli.sh
+++ infrastructure/ansible/roles/eap/templates/lms_trans_cli.sh
@@ -2,9 +2,9 @@ if (outcome != success) of /subsystem=datasources/jdbc-driver=mysql:read-attribu
   /subsystem=datasources/jdbc-driver=mysql:add(driver-xa-datasource-class-name=com.mysql.jdbc.jbdc2.optional.MysqlXAConnection,driver-name=mysql,driver-module-name=com.mysql)
 end-if
 
-if (outcome != success) of /subsystem=datasources/data-source=lmsTransDS:read-attribute(name=enabled)
-  # lmsTransDS
-  data-source add --name=lmsTransDS --jndi-name="java:jboss/datasources/lmsTransDS" --driver-name=mysql --user-name=@@lms_trans_userId@@ --password=@@lms_trans_passwd@@ --exception-sorter-class-name=org.jboss.jca.adapters.jdbc.extensions.mysql.MySQLExceptionSorter --valid-connection-checker-class-name=org.jboss.jca.adapters.jdbc.extensions.mysql.MySQLValidConnectionChecker --connection-url="jdbc:mysql://${mysql.host.ip}:${mysql.host.port}/${lms_trans_db_name}?transformedBitIsBoolean=true&sessionVariables=storage_engine=InnoDB"
-  /subsystem=datasources/data-source=lmsTransDS:enable
+if (outcome != success) of /subsystem=datasources/data-source=transactionalDS:read-attribute(name=enabled)
+  # transactionalDS
+  data-source add --name=transactionalDS --jndi-name="java:jboss/datasources/transactionalDS" --driver-name=mysql --user-name=@@lms_trans_userId@@ --password=@@lms_trans_passwd@@ --exception-sorter-class-name=org.jboss.jca.adapters.jdbc.extensions.mysql.MySQLExceptionSorter --valid-connection-checker-class-name=org.jboss.jca.adapters.jdbc.extensions.mysql.MySQLValidConnectionChecker --connection-url="jdbc:mysql://${mysql.host.ip}:${mysql.host.port}/${lms_trans_db_name}?transformedBitIsBoolean=true&sessionVariables=storage_engine=InnoDB"
+  /subsystem=datasources/data-source=transactionalDS:enable
 
 end-if
