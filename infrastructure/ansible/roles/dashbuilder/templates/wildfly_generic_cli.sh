943cc381d0d51ae9ad8ef1328aee3a8acd2ec39c
jbride <jbride2001@yahoo.com>
2016-12-26 10:10:58 -0700

    https://github.com/redhat-gpe/OPEN_Reporting/issues/87

--- infrastructure/ansible/roles/dashbuilder/templates/wildfly_generic_cli.sh
+++ infrastructure/ansible/roles/dashbuilder/templates/wildfly_generic_cli.sh
@@ -1,2 +1,11 @@
 # Create JBoss EAP module based on jdbc driver library installed from RHN
 module add --name=@@lms_reporting_module_name@@ --resources=@@lms_reporting_driver_jar_dir@@/@@lms_reporting_driver_jar@@ --dependencies=javax.api,javax.transaction.api
+
+batch
+
+/subsystem=logging/logger=com.redhat.ge.reporting/:add(category=com.redhat.ge.reporting,level=DEBUG)
+/subsystem=logging/logger=org.jboss.security.auth.spi.AbstractServerLoginModule/:add(category=org.jboss.security.auth.spi.AbstractServerLoginModule,level=TRACE)
+/subsystem=logging/logger=org.jboss.security/:add(category=org.jboss.security,level=INFO)
+
+
+run-batch
