d6527a615d58d8f4d46aa2cbf0f0d56232a6c1fd
jbride <jbride2001@yahoo.com>
2017-01-30 13:54:37 -0700

    https://github.com/redhat-gpe/OPEN_Reporting/issues/198

--- infrastructure/ansible/roles/mariadb/files/install/run-mysqld.sh
+++ infrastructure/ansible/roles/mariadb/files/install/run-mysqld.sh
@@ -7,9 +7,6 @@ source $SCRIPT_DIR/scripts/common.sh
 
 set -eu
 
-rm -f $MYSQL_DEFAULTS_FILE
-envsubst < ${SCRIPT_DIR}/scripts/$(basename $MYSQL_DEFAULTS_FILE).template > $MYSQL_DEFAULTS_FILE 
-  
 initialize_database
 shutdown_local_mysql
 unset_env_vars  
