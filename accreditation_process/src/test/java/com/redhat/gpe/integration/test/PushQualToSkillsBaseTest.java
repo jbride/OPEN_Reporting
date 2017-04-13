9e4ca2db856108d73dd6d78d2b4719c59275adb5
jbride <jbride2001@yahoo.com>
2017-04-12 21:14:59 -0600

    https://github.com/redhat-gpe/OPEN_Reporting/issues/221

--- accreditation_process/src/test/java/com/redhat/gpe/integration/test/PushQualToSkillsBaseTest.java
+++ accreditation_process/src/test/java/com/redhat/gpe/integration/test/PushQualToSkillsBaseTest.java
@@ -3,6 +3,7 @@ package com.redhat.gpe.integration.test;
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.Iterator;
 
 import org.apache.camel.Endpoint;
 import org.apache.camel.Exchange;
@@ -16,6 +17,9 @@ import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import com.redhat.gpte.util.PropertiesSupport;
 
@@ -31,6 +35,8 @@ public class PushQualToSkillsBaseTest extends CamelSpringTestSupport {
     private static final Logger logger = LoggerFactory.getLogger(PushQualToSkillsBaseTest.class);
     private String pushQualToSkillsBaseURI = "direct:push-qual-to-skills-base";
     private String pushQualsToSkillsBaseBatchURI = "direct:push-qual-to-skillsbase-uri";
+
+    private String personQualifications = "{\"status\":\"success\",\"data\":[{\"name\":\"Bachelor of Science in Material Science Engineering\",\"person_id\":295,\"status\":\"completed\",\"start_date\":\"Sep-03-1990\",\"end_date\":null},{\"name\":\"Red Hat Advanced Delivery Specialist - Cloud Management\",\"person_id\":295,\"status\":\"completed\",\"start_date\":\"Apr-12-2017\",\"end_date\":\"Apr-12-2019\"},{\"name\":\"Red Hat Advanced Delivery Specialist - Cloud Management\",\"person_id\":295,\"status\":\"completed\",\"start_date\":\"Apr-12-2017\",\"end_date\":\"Apr-12-2019\"},{\"name\":\"Red Hat Advanced Delivery Specialist - Cloud Management\",\"person_id\":295,\"status\":\"completed\",\"start_date\":\"Apr-12-2017\",\"end_date\":\"Apr-12-2019\"},{\"name\":\"Red Hat Advanced Delivery Specialist - Cloud Management\",\"person_id\":295,\"status\":\"completed\",\"start_date\":\"Apr-12-2017\",\"end_date\":\"Apr-12-2019\"},{\"name\":\"Red Hat Advanced Delivery Specialist - Cloud Management\",\"person_id\":295,\"status\":\"completed\",\"start_date\":\"Apr-12-2017\",\"end_date\":\"Apr-12-2019\"},{\"name\":\"Red Hat Advanced Delivery Specialist - Cloud Management\",\"person_id\":295,\"status\":\"completed\",\"start_date\":\"Apr-12-2017\",\"end_date\":\"Apr-12-2019\"}]}";
     
     public PushQualToSkillsBaseTest() throws IOException {
         PropertiesSupport.setupProps();
@@ -46,6 +52,23 @@ public class PushQualToSkillsBaseTest extends CamelSpringTestSupport {
         return new ClassPathXmlApplicationContext("/spring/accreditation-camel-context.xml");
     }
 
+    //@Ignore
+    @Test
+    public void testPersonQualificationsParsing() throws org.json.JSONException {
+        JSONObject jsonResponse = new JSONObject(personQualifications);
+        JSONArray jArray = jsonResponse.optJSONArray("data");
+        for(int i=0; i < jArray.length(); i++) {
+            JSONObject jObj = jArray.getJSONObject(i);
+            String END_DATE = "end_date";
+            String NAME = "name";
+            String STATUS = "status";
+            String START_DATE = "start_date";
+
+            String existingQual = jObj.getString(NAME);
+            System.out.println("testPersonQualificationsParsing() existingQual = "+existingQual+"\n\tobj = "+jObj.toString());
+        }
+    }
+
     @Ignore
     @Test
     public void testQueryForUnpushedQuals() throws InterruptedException {
@@ -58,7 +81,7 @@ public class PushQualToSkillsBaseTest extends CamelSpringTestSupport {
         template.send(pushQualsToSkillsBaseBatchURI, exchange);
     }
 
-    //@Ignore
+    @Ignore
     @Test
     public void testPushQualToSkillsBaseGivenKnownRHTStudent() throws InterruptedException {
 
