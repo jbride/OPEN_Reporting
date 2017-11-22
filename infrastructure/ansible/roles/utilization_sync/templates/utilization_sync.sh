b4eefee83577b39553bf5a5fe82010289b5003df
jbride <jbride2001@yahoo.com>
2017-11-22 13:17:01 -0700

    https://github.com/redhat-gpe/OPEN_Reporting/issues/275

---
+++ infrastructure/ansible/roles/utilization_sync/templates/utilization_sync.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export PGPASSWORD={{ utilization_db_password }}
+
+echo -en "PGPASSWORD=$PGPASSWORD\n"
+
+echo -en "pull utilization data: $1\n"
+psql -h {{ utilization_db_host }} -U {{ utilization_db_username  }} -w $1 -c "\copy provisioned_services TO '{{database_update_script_dir}}/$1-db.csv' CSV DELIMITER ','"
+
+echo -en "load utilization data: $1\n"
+mysql -u root {{lms_reporting_db_name}} -e "load data local infile '{{database_update_script_dir}}/$1-db.csv' into table lms_reporting.RhpdsUsage fields terminated by ',' lines terminated by '\n';"
