0e7300043a2b89b4f2536798a795fc57549703d5
jbride <jbride2001@yahoo.com>
2017-12-04 11:52:01 -0700

    https://github.com/redhat-gpe/OPEN_Reporting/issues/276

--- infrastructure/ansible/roles/utilization_sync/templates/utilization_sync.sh
+++ infrastructure/ansible/roles/utilization_sync/templates/utilization_sync.sh
@@ -2,16 +2,17 @@
 
 export PGPASSWORD={{ utilization_db_password }}
 
-echo -en "PGPASSWORD=$PGPASSWORD\n"
+echo -en "utilization_sync" > $2
+echo -en "PGPASSWORD=$PGPASSWORD\n" >> $2
 
-echo -en "pull utilization data: $1\n"
+echo -en "pull utilization data: $1\n" >> $2
 psql -h {{ utilization_db_host }} -U {{ utilization_db_username  }} -w $1 -c "\copy provisioned_services TO '{{database_update_script_dir}}/$1-db.csv' CSV DELIMITER ','"
 if [ $? != 0 ];
 then
     exit 1;
 fi
 
-echo -en "load utilization data: $1\n"
+echo -en "load utilization data: $1\n" >> $2
 mysql -u root {{lms_reporting_db_name}} -e "truncate table lms_reporting.$1;"
 if [ $? != 0 ];
 then
@@ -24,7 +25,7 @@ then
     exit 1;
 fi
 
-echo -en "run procedure to update dashbuilder"
+echo -en "run procedure to update dashbuilder" >> $2
 mysql -u root {{lms_reporting_db_name}} -e "call refresh_labs_demos;"
 if [ $? != 0 ];
 then
