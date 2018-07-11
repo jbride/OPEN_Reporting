378420d1dc6585e71d96908cf038ca39ebdf26be
svmangan <smangan@redhat.com>
2018-07-11 12:42:55 -0400

    https://github.com/redhat-gpe/OPEN_Reporting/issues/305

--- db_scripts/lms_transactional_ddl.sql
+++ db_scripts/lms_transactional_ddl.sql
@@ -106,7 +106,7 @@ CREATE TABLE `Countries` (
   `CreateDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`CountryID`),
   UNIQUE KEY `IDX_CountryName` (`CountryName`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -486,4 +486,4 @@ CREATE TABLE `SumtotalCountries` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-11 12:25:43
+-- Dump completed on 2018-07-11 12:42:32
